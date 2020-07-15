d2d8a35a387f4813f33d02bfd1ac4fcf5b11b4c9
Seb943 <55142014+Seb943@users.noreply.github.com>
2020-07-15 15:05:35 +0200

    Update FinalScraper.py

--- FinalScraper.py
+++ FinalScraper.py
@@ -4,7 +4,6 @@
 ''' Create 4 main functions : scrape_historical, scrape_specific_season, scrape current_season, scrape_next_games'''
 import os
 
-os.chdir("C:\\Users\\Sébastien CARARO\\Desktop\\ATP& &Others\\WebScraping")
 from functions import *
 #scrape_oddsportal_historical(sport = 'soccer', country = 'france', league = 'ligue-1', start = '2010-2011', nseason = 5, current_season = 'yes')
 #scrape_oddsportal_current_season(sport = 'soccer', country = 'finland', league = 'veikkausliiga', season = '2020')
