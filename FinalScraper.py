7bbeac018d886b12ac00a7afe0b6422250bb9821
Seb943 <55142014+Seb943@users.noreply.github.com>
2020-11-14 10:19:24 +0100

    Add files via upload

--- FinalScraper.py
+++ FinalScraper.py
@@ -1,7 +1,8 @@
-############ Final oddsportal scraper 
+############ Final oddsportal scraper
 
 # ATP, baseball, basket, darts, eSports, football, nfl, nhl, rugby
-''' Create 4 main functions : scrape_historical, scrape_specific_season, scrape current_season, scrape_next_games'''
+''' Create 4 main functions : scrape_historical, scrape_specific_season, scrape current_season, scrape_next_games
+NB : You need to be in the right repository to import functions...'''
 import os
 
 #os.chdir("C:\\Users\\Sébastien CARARO\\Desktop\\ATP& &Others\\WebScraping")
